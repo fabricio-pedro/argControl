7728d11a22203c487808252ba0f4cabfdde75bba
=fabricio-pedro <wolfxzero2@gmail.com>
2020-05-14 02:57:05 -0300

    project arfControl initial

---
+++ src/main/java/br/com/imaginautSoft/ArgControlApplication.java
@@ -0,0 +1,13 @@
+package br.com.imaginautSoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArgControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArgControlApplication.class, args);
+	}
+
+}
